d039800c1dee4fb22d2e07a250e769ec5663b647
toxicpapa00 <gyaneshpatel605@gmail.com>
2025-11-24 11:34:35 +0530

    Uploaded via Bot

---
+++ REVANGEMUSIC/plugins/REVANGE/watcher.py
@@ -0,0 +1,30 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from REVANGEMUSIC import app
+from REVANGEMUSIC.core.call import Sona
+
+welcome = 20
+close = 30
+
+
+@app.on_message(filters.video_chat_started, group=welcome)
+@app.on_message(filters.video_chat_ended, group=close)
+async def welcome(_, message: Message):
+    await Sona.stop_stream_force(message.chat.id)
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
