025f1b9c7fd750938be48ff4bdd6f870a81bf41b
toxicpapa00 <gyaneshpatel605@gmail.com>
2025-11-24 11:33:36 +0530

    Uploaded via Bot

---
+++ REVANGEMUSIC/plugins/tools/sticker.py
@@ -0,0 +1,181 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
+
+import base64, os, httpx, cv2, pyrogram 
+from config import BOT_USERNAME
+from REVANGEMUSIC import app
+from uuid import uuid4
+from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
+from PIL import Image
+from pyrogram import Client, filters
+
+
+@app.on_message(filters.command("packkang"))
+async def _packkang(app :app,message):  
+    txt = await message.reply_text("**ᴘʀᴏᴄᴇssɪɴɢ....**")
+    if not message.reply_to_message:
+        await txt.edit('ʀᴇᴘʟʏ ᴛᴏ ᴍᴇssᴀɢᴇ')
+        return
+    if not message.reply_to_message.sticker:
+        await txt.edit('ʀᴇᴘʟʏ ᴛᴏ sᴛɪᴄᴋᴇʀ')
+        return
+    if message.reply_to_message.sticker.is_animated or  message.reply_to_message.sticker.is_video:
+        return await txt.edit("ʀᴇᴘʟʏ ᴛᴏ ᴀ ɴᴏɴ-ᴀɴɪᴍᴀᴛᴇᴅ sᴛɪᴄᴋᴇʀ")
+    if len(message.command) < 2:
+        pack_name =  f'{message.from_user.first_name}_sticker_pack_by_@{BOT_USERNAME}'
+    else :
+        pack_name = message.text.split(maxsplit=1)[1]
+    short_name = message.reply_to_message.sticker.set_name
+    stickers = await app.invoke(
+        pyrogram.raw.functions.messages.GetStickerSet(
+            stickerset=pyrogram.raw.types.InputStickerSetShortName(
+                short_name=short_name),
+            hash=0))
+    shits = stickers.documents
+    sticks = []
+    
+    for i in shits:
+        sex = pyrogram.raw.types.InputDocument(
+                id=i.id,
+                access_hash=i.access_hash,
+                file_reference=i.thumbs[0].bytes
+            )
+        
+        sticks.append(
+            pyrogram.raw.types.InputStickerSetItem(
+                document=sex,
+                emoji=i.attributes[1].alt
+            )
+        )
+
+    try:
+        short_name = f'stikcer_pack_{str(uuid4()).replace("-","")}_by_{app.me.username}'
+        user_id = await app.resolve_peer(message.from_user.id)
+        await app.invoke(
+            pyrogram.raw.functions.stickers.CreateStickerSet(
+                user_id=user_id,
+                title=pack_name,
+                short_name=short_name,
+                stickers=sticks,
+            )
+        )
+        await txt.edit(f"**ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴋᴀɴɢᴇᴅ ʟɪɴᴋ**!\n\n**ᴛᴏᴛᴀʟ sᴛɪᴄᴋᴇʀ **:- {len(sticks)}",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ᴘᴀᴄᴋ ʟɪɴᴋ",url=f"http://t.me/addstickers/{short_name}")]]))
+    except Exception as e:
+        await message.reply(str(e))
+
+
+@app.on_message(filters.command(["stickerid","stid"]))
+async def sticker_id(app: app, msg):
+    if not msg.reply_to_message:
+        await msg.reply_text("Reply to a sticker")        
+    elif not msg.reply_to_message.sticker:
+        await msg.reply_text("Reply to a sticker")        
+    st_in = msg.reply_to_message.sticker
+    await msg.reply_text(f"""
+⊹ <u>**sᴛɪᴄᴋᴇʀ ɪɴғᴏ**</u> ⊹\n\n
+**⊚ sᴛɪᴄᴋᴇʀ ɪᴅ **: `{st_in.file_id}`\n
+**⊚ sᴛɪᴄᴋᴇʀ ᴜɴɪǫᴜᴇ ɪᴅ **: `{st_in.file_unique_id}`
+""")
+
+
+@app.on_message(filters.command("st"))
+async def generate_sticker(client, message):
+    if len(message.command) == 2:
+        sticker_id = message.command[1]
+        try:
+            await client.send_sticker(message.chat.id, sticker=sticker_id)
+        except Exception as e:
+            await message.reply_text(f"**ᴇʀʀᴏʀ :-** `{e}`")
+    else:
+        await message.reply_text(
+            "**ᴘʀᴏᴠɪᴅᴇ ᴀ sᴛɪᴄᴋᴇʀ ɪᴅ ᴀғᴛᴇʀ ᴄᴏᴍᴍᴀɴᴅ.**\n\n"
+            "**ᴜsᴀɢᴇ :-** `/st sticker_id`"
+        )
+
+
+@app.on_message(filters.command("tiny"))
+async def tiny_sticker(client, message):
+    reply = message.reply_to_message
+    if not (reply and reply.sticker):
+        await message.reply("**» ᴘʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀ sᴛɪᴄᴋᴇʀ**")
+        return
+    kontol = await message.reply("**» ᴘʀᴏᴄᴇssɪɴɢ ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...**")
+    await kontol.edit_text("🐾")
+    ik = await app.download_media(reply)
+    im1 = Image.open("REVANGEMUSIC/assets/sonali.png")
+    if ik.endswith(".tgs"):
+        await app.download_media(reply, "wel2.tgs")
+        os.system("lottie_convert.py wel2.tgs json.json")
+        with open("json.json", "r") as json_file:
+            jsn = json_file.read()
+            jsn = jsn.replace("512", "2000")
+        with open("json.json", "w") as json_file:
+            json_file.write(jsn)
+        os.system("lottie_convert.py json.json wel2.tgs")
+        file = "wel2.tgs"
+        os.remove("json.json")
+    elif ik.endswith((".gif", ".mp4")):
+        iik = cv2.VideoCapture(ik)
+        _, busy = iik.read()
+        cv2.imwrite("i.png", busy)
+        fil = "i.png"
+        im = Image.open(fil)
+        z, d = im.size
+        if z == d:
+            xxx, yyy = 200, 200
+        else:
+            t = z + d
+            a = z / t
+            b = d / t
+            aa = (a * 100) - 50
+            bb = (b * 100) - 50
+            xxx = 200 + 5 * aa
+            yyy = 200 + 5 * bb
+        k = im.resize((int(xxx), int(yyy)))
+        k.save("k.png", format="PNG", optimize=True)
+        im2 = Image.open("k.png")
+        back_im = im1.copy()
+        back_im.paste(im2, (150, 0))
+        back_im.save("o.webp", "WEBP", quality=95)
+        file = "o.webp"
+        os.remove(fil)
+        os.remove("k.png")
+    else:
+        im = Image.open(ik)
+        z, d = im.size
+        if z == d:
+            xxx, yyy = 200, 200
+        else:
+            t = z + d
+            a = z / t
+            b = d / t
+            aa = (a * 100) - 50
+            bb = (b * 100) - 50
+            xxx = 200 + 5 * aa
+            yyy = 200 + 5 * bb
+        k = im.resize((int(xxx), int(yyy)))
+        k.save("k.png", format="PNG", optimize=True)
+        im2 = Image.open("k.png")
+        back_im = im1.copy()
+        back_im.paste(im2, (150, 0))
+        back_im.save("o.webp", "WEBP", quality=95)
+        file = "o.webp"
+        os.remove("k.png")
+    await app.send_document(message.chat.id, file, reply_to_message_id=message.id)
+    await kontol.delete()
+    os.remove(file)
+    os.remove(ik)
+
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
