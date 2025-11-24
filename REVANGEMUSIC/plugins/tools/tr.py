3d77d287139af5f8af9248564061cb65ad4b330b
toxicpapa00 <gyaneshpatel605@gmail.com>
2025-11-24 11:33:29 +0530

    Uploaded via Bot

---
+++ REVANGEMUSIC/plugins/tools/tr.py
@@ -0,0 +1,79 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
+
+from pyrogram.types import *
+from REVANGEMUSIC import app
+from gpytranslate import Translator
+from pyrogram import Client, filters
+from gtts import gTTS
+
+
+trans = Translator()
+
+
+@app.on_message(filters.command("tr"))
+async def translate(_, message) -> None:
+    reply_msg = message.reply_to_message
+    if not reply_msg:
+        await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴛʀᴀɴsʟᴀᴛᴇ ɪᴛ !")
+        return
+    if reply_msg.caption:
+        to_translate = reply_msg.caption
+    elif reply_msg.text:
+        to_translate = reply_msg.text
+    try:
+        args = message.text.split()[1].lower()
+        if "//" in args:
+            source = args.split("//")[0]
+            dest = args.split("//")[1]
+        else:
+            source = await trans.detect(to_translate)
+            dest = args
+    except IndexError:
+        source = await trans.detect(to_translate)
+        dest = "en"
+    translation = await trans(to_translate, sourcelang=source, targetlang=dest)
+    reply = (
+        f"ᴛʀᴀɴsʟᴀᴛᴇᴅ ғʀᴏᴍ {source} to {dest}:\n"
+        f"{translation.text}"
+    )
+    await message.reply_text(reply)
+
+
+@app.on_message(filters.command('tts'))
+async def text_to_speech(client, message):
+    try:
+      
+        if len(message.text.split()) < 2:
+            await message.reply_text(
+                "**ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴛᴇxᴛ ғᴏʀ ᴛᴛs.** \n\n**ᴜsᴀɢᴇ :** `/tts i love you`"
+            )
+            return
+
+        
+        text = message.text.split(' ', 1)[1]
+
+        
+        tts = gTTS(text=text, lang='hi')
+        file_name = "speech.mp3"
+        tts.save(file_name)
+
+        
+        await app.send_audio(chat_id=message.chat.id, audio=file_name)
+
+    except Exception as e:
+        
+        await message.repl
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
