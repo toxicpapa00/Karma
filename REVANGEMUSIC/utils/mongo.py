e682484a4e40394225cfa6b8592832ad07234179
toxicpapa00 <gyaneshpatel605@gmail.com>
2025-11-24 11:35:21 +0530

    Uploaded via Bot

---
+++ REVANGEMUSIC/utils/mongo.py
@@ -0,0 +1,63 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
+
+from typing import Dict, Union
+
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+
+from config import MONGO_DB_URI
+
+mongo = MongoCli(MONGO_DB_URI)
+db = mongo.REVANGEMUSIC
+
+coupledb = db.couple
+
+afkdb = db.afk
+
+notesdb = db.notes
+
+async def _get_lovers(cid: int):
+    lovers = await coupledb.find_one({"chat_id": cid})
+    if lovers:
+        lovers = lovers["couple"]
+    else:
+        lovers = {}
+    return lovers
+
+async def _get_image(cid: int):
+    lovers = await coupledb.find_one({"chat_id": cid})
+    if lovers:
+        lovers = lovers["img"]
+    else:
+        lovers = {}
+    return lovers
+
+async def get_couple(cid: int, date: str):
+    lovers = await _get_lovers(cid)
+    if date in lovers:
+        return lovers[date]
+    else:
+        return False
+
+
+async def save_couple(cid: int, date: str, couple: dict, img: str):
+    lovers = await _get_lovers(cid)
+    lovers[date] = couple
+    await coupledb.update_one(
+        {"chat_id": cid},
+        {"$set": {"couple": lovers, "img": img}},
+        upsert=True,
+    )
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
