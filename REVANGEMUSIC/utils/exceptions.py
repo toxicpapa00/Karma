2156fc4b26fdceb93cc1fda1f96826311d499c13
toxicpapa00 <gyaneshpatel605@gmail.com>
2025-11-24 11:35:22 +0530

    Uploaded via Bot

---
+++ REVANGEMUSIC/utils/exceptions.py
@@ -0,0 +1,19 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
+
+class AssistantErr(Exception):
+    def __init__(self, errr: str):
+        super().__init__(errr)
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
