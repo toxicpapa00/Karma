2f1ae5a29c41ba3c32c2cce7a17c3617312bf298
toxicpapa00 <gyaneshpatel605@gmail.com>
2025-11-24 11:34:57 +0530

    Uploaded via Bot

---
+++ REVANGEMUSIC/utils/extraction.py
@@ -0,0 +1,33 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
+
+from pyrogram.enums import MessageEntityType
+from pyrogram.types import Message, User
+
+from REVANGEMUSIC import app
+
+
+async def extract_user(m: Message) -> User:
+    if m.reply_to_message:
+        return m.reply_to_message.from_user
+    msg_entities = m.entities[1] if m.text.startswith("/") else m.entities[0]
+    return await app.get_users(
+        msg_entities.user.id
+        if msg_entities.type == MessageEntityType.TEXT_MENTION
+        else int(m.command[1])
+        if m.command[1].isdecimal()
+        else m.command[1]
+    )
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
