8f2dc9f55d3554b3d865c75919b1de65c5172596
toxicpapa00 <gyaneshpatel605@gmail.com>
2025-11-24 11:35:39 +0530

    Uploaded via Bot

---
+++ REVANGEMUSIC/utils/inline/start.py
@@ -0,0 +1,51 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
+
+from pyrogram.types import InlineKeyboardButton
+
+import config
+from REVANGEMUSIC import app
+
+
+def start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+        ],
+    ]
+    return buttons
+
+
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton("˹ ᴧʙᴏᴜᴛ ˼", callback_data="ALLBOT_CP"),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="MAIN_CP"),
+        ],
+    ]
+    return buttons
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
