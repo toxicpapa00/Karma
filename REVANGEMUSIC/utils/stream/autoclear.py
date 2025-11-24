1acb1f7b700dc3dc93973bba7d6495e405e81d0d
toxicpapa00 <gyaneshpatel605@gmail.com>
2025-11-24 11:35:28 +0530

    Uploaded via Bot

---
+++ REVANGEMUSIC/utils/stream/autoclear.py
@@ -0,0 +1,34 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
+
+import os
+
+from config import autoclean
+
+
+async def auto_clean(popped):
+    try:
+        rem = popped["file"]
+        autoclean.remove(rem)
+        count = autoclean.count(rem)
+        if count == 0:
+            if "vid_" not in rem or "live_" not in rem or "index_" not in rem:
+                try:
+                    os.remove(rem)
+                except:
+                    pass
+    except:
+        pass
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
