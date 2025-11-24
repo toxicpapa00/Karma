6ceb4e82e1df852eeb3093ba7023ba630d8dc8ee
toxicpapa00 <gyaneshpatel605@gmail.com>
2025-11-24 11:34:29 +0530

    Uploaded via Bot

---
+++ REVANGEMUSIC/plugins/admins/stop.py
@@ -0,0 +1,39 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from REVANGEMUSIC import app
+from REVANGEMUSIC.core.call import Sona
+from REVANGEMUSIC.utils.database import set_loop
+from REVANGEMUSIC.utils.decorators import AdminRightsCheck
+from REVANGEMUSIC.utils.inline import close_markup
+from config import BANNED_USERS
+
+
+@app.on_message(
+    filters.command(["end", "stop", "cend", "cstop"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.group & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def stop_music(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return
+    await Sona.stop_stream(chat_id)
+    await set_loop(chat_id, 0)
+    await message.reply_text(
+        _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
+    )
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
