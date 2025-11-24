90cbf3b49e8a4cfd373040bd97d558692f3a60cd
toxicpapa00 <gyaneshpatel605@gmail.com>
2025-11-24 11:35:33 +0530

    Uploaded via Bot

---
+++ REVANGEMUSIC/utils/decorators/__init__.py
@@ -0,0 +1,18 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
+
+from .admins import *
+from .language import *
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Revange 😎
+
+# 🧑‍💻 Developer : t.me/dmcatelegram
+# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
+# 📢 Telegram channel : t.me/dmcatelegram
+# =======================================================
